1d469cd89b4b5124f0e906e315c91c9c0f472f87
frank <frank123123412345@gmail.com>
2021-02-10 18:14:49 +0400

    add remote log module in globalserver

--- globalserver.go
+++ globalserver.go
@@ -1,15 +1,39 @@
 package main
 
 import (
+	logModule "globalserver/module/log"
 	"globalserver/module/platform"
 	"jarvis/base/database"
 	"jarvis/base/database/redis"
-	uTime "jarvis/util/time"
 	"jarvis/base/log"
+	"jarvis/base/network"
+	uTime "jarvis/util/time"
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
 )
 
+const (
+	// 自定义服务最大连接数量
+	CustomMaxConnection = 5000
+	// 自定义服务消息管道大小
+	CustomIntoStreamSize = 10000
+	// 日志 Socket 接收地址
+	LogSocketAddress = ":10000"
+)
+
+var (
+	service network.Service
+)
+
 func init() {
+	// 实例化服务
+	service = network.NewService(
+		CustomMaxConnection,
+		CustomIntoStreamSize,
+	)
+
 	// 初始化 MySQL
 	if err := database.InitializeMySQL(
 		"frank",
@@ -41,10 +65,35 @@ func init() {
 }
 
 func main() {
-	closeChannel := make(chan struct{})
-
+	// 1.启动全局平台信息更新
 	ticker := uTime.NewTicker(time.Duration(30)*time.Minute, platform.LoadPlatformInfo)
 	ticker.Run()
 
-	<-closeChannel
+	// 2.注册模块
+	if err := service.RegisterModule(logModule.NewModule()); err != nil {
+		log.ErrorF("Register module error : %s", err)
+		return
+	}
+
+	// 3.启动
+	if err := service.Run(
+		network.NewSocketGate(LogSocketAddress), // Socket 入口
+	); err != nil {
+		log.ErrorF("Register observer error : %s", err)
+		return
+	}
+
+	// 4.监听系统信号
+	monitorSystemSignal()
+}
+
+// 监听系统信号
+// kill -SIGQUIT [进程号] : 杀死当前进程
+func monitorSystemSignal() {
+	sc := make(chan os.Signal)
+	signal.Notify(sc, syscall.SIGQUIT)
+	select {
+	case <-sc:
+		log.InfoF("Done")
+	}
 }
